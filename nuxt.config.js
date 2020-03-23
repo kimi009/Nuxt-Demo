8ce05b452bc2514fd81e363689f7c857b58aa47d
kimi009 <444812002@qq.com>
2020-03-23 16:44:05 +0800

    test

---
+++ nuxt.config.js
@@ -0,0 +1,93 @@
+module.exports = {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  // css: ['ant-design-vue/dist/antd.css'],
+  css: [
+    'ant-design-vue/lib/row/style/css',
+    'ant-design-vue/lib/col/style/css',
+    'ant-design-vue/lib/button/style/css',
+    'ant-design-vue/lib/menu/style/css',
+    'ant-design-vue/lib/table/style/css',
+    'ant-design-vue/lib/tag/style/css',
+    'ant-design-vue/lib/divider/style/css',
+    'ant-design-vue/lib/card/style/css'
+  ],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/antd-ui'],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // config.resolve.alias['@ant-design/icons/lib/dist$'] = path.resolve(
+      //   __dirname,
+      //   './assets/icon/antd-icon.js'
+      // ) // 引入需要的
+      // config.module.rules.push({
+      //   test: /\.less$/,
+      //   use: [
+      //     {
+      //       loader: 'less-loader',
+      //       options: {
+      //         // modifyVars: {
+      //         //   'primary-color': '#2EA9DF',
+      //         //   'link-color': '#2EA9DF',
+      //         //   'border-radius-base': '4px'
+      //         // },
+      //         javascriptEnabled: true
+      //       }
+      //     }
+      //   ]
+      // })
+    }
+    // productionSourceMap: false,
+    // productionGzip: true,
+    // productionGzipExtensions: ['js', 'css']
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="page-index">
+    <a-menu v-model="current" mode="horizontal" theme="dark">
+      <a-menu-item key="mail">
+        <a-icon type="mail" />Navigation One
+      </a-menu-item>
+      <a-menu-item key="app">
+        <a-icon type="appstore" />Navigation Two
+      </a-menu-item>
+      <a-sub-menu>
+        <span slot="title" class="submenu-title-wrapper"
+          ><a-icon type="setting" />Navigation Three - Submenu</span
+        >
+        <a-menu-item-group title="Item 1">
+          <a-menu-item key="setting:1">Option 1</a-menu-item>
+          <a-menu-item key="setting:2">Option 2</a-menu-item>
+        </a-menu-item-group>
+        <a-menu-item-group title="Item 2">
+          <a-menu-item key="setting:3">Option 3</a-menu-item>
+          <a-menu-item key="setting:4">Option 4</a-menu-item>
+        </a-menu-item-group>
+      </a-sub-menu>
+      <a-menu-item key="alipay">
+        <a href="https://antdv.com" target="_blank" rel="noopener noreferrer"
+          >Navigation Four - Link</a
+        >
+      </a-menu-item>
+    </a-menu>
+    <a-row :gutter="16">
+      <template v-for="item in list">
+        <a-col :key="item.id" :span="8">
+          <a-card :title="item.name" :bordered="false">
+            <p>{{ item.des }}</p>
+          </a-card>
+        </a-col>
+      </template>
+    </a-row>
+    <a-button type="primary" @click="toDetail">跳页面测试</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  async asyncData({ $axios }) {
+    const {
+      status,
+      data: { list }
+    } = await $axios.get('/user/info')
+    if (status === 200) {
+      return { list }
+    }
+    return { list: [] }
+  },
+  data() {
+    return {
+      current: ['mail']
+    }
+  },
+  methods: {
+    toDetail() {
+      location.href = '/users/111'
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.page-index {
+  // .ant-menu {
+  //   background-color: #000000;
+  // }
+  // .ant-menu-submenu {
+  //   color: #ffffff;
+  // }
+  // .ant-menu-item-selected {
+  //   color: #1890ff;
+  // }
+  // .ant-menu-item {
+  //   a {
+  //     color: #ffffff;
+  //   }
+  // }
+}
+</style>
